66d2f256ec9519b1e47913fb68fac9c7b984e55c
Mike Richards <richardsmichael94@gmail.com>
2019-01-21 17:02:09 +0000

    Installed devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
-  get 'about', to: 'pages#about'
-  resources :contacts, only: [:create]
-  get 'contact-us', to: 'contacts#new', as: 'new_contact' 
-end
-Rails.application.routes.draw do
+  devise_for :users
   root to: "pages#home"
   get 'about', to: 'pages#about'
   resources :contacts, only: [:create]
