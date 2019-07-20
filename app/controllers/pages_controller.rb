4be7fcc1f2cb9945a892dcb997190e750b22e159
richardsmichael94@gmail.com <richardsmichael94@gmail.com>
2019-07-20 01:23:59 +0000

    Added paths and skeleton pages for all offering options with image and title links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,10 +11,25 @@ class PagesController < ApplicationController
   def public_speaking
   end
   
-  def serve
+  def nonverbal
   end 
   
   def business_writing
+  end 
+  
+  def group_dynamics
+  end 
+  
+  def intercultural_communication
+  end 
+  
+  def editing
+  end 
+  
+  def interviewing
+  end 
+  
+  def business_etiquette
   end 
 
 end 
